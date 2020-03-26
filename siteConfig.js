aee079c7a5c8b68eff97816a1b24c3207f05b7c1
Promisemasimba <promisemasimba411@gmail.com>
2020-03-26 23:48:34 +0200

    building site

--- siteConfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'Citrix Digital',
-  shortName: 'Cd.',
-  description: 'Helping People Through Technology',
-  url: 'https://citrixdigital.com', // Domain of your site without prefix!
-  prefix: '/',
-  author: 'Promise Masimba',
-  twitter: 'Promise_Masimba'
-}
