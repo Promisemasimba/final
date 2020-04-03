114d02507dc64a35540fc08f5e68c822fbbe665b
Promisemasimba <promisemasimba411@gmail.com>
2020-04-03 23:57:25 +0200

    form

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,6 +14,67 @@ const AboutPage = ({ data }, location) => {
   return (
     <Layout title={siteTitle}>
       <SEO title="About" keywords={[`citrix digital`, `tech`, `web`, `hosting`, `wordpress`]} />
+                <form
+          name="contact"
+          method="post"
+          action="/contact/thanks/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+          >
+            <div className="row gtr-uniform">
+              <div className="col-6 col-12-xsmall">
+                <input
+                  type="text"
+                  name="Name"
+                  onChange={this.handleChange}
+                  id="name"
+                  defaultValue
+                  placeholder="Name"
+                  required={true}
+                />
+              </div>
+              <div className="col-6 col-12-xsmall">
+                <input
+                  type="email"
+                  name="Email"
+                  onChange={this.handleChange}
+                  id="email"
+                  defaultValue
+                  placeholder="Email"
+                  required={true}
+                />
+              </div>
+              {/* Break */}
+              <div className="col-12">
+                <textarea
+                  name="message"
+                  onChange={this.handleChange}
+                  id="message"
+                  placeholder="Enter your message"
+                  rows={6}
+                  defaultValue={""}
+                  required={true}
+                />
+              </div>
+              {/* Break */}
+              <div className="col-12">
+                <ul className="actions">
+                  <li>
+                    <input
+                      type="submit"
+                      defaultValue="Send Message"
+                      className="primary"
+                    />
+                  </li>
+                  <li>
+                    <input type="reset" defaultValue="Reset" />
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </form>
+    
 
     
     </Layout>
